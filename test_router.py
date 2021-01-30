b59ba5ecfa0bf1701cbee548e6f073b3f41e77ce
wut5238 <setthawut9915@hotmail.com>
2021-01-30 13:52:49 +0000

    modify test_router.py

--- test_router.py
+++ test_router.py
@@ -6,8 +6,7 @@ class test_router(unittest.TestCase):
         '''test all atribute in router.py'''
         self.assertTrue(self.__init__)
     def test_method(self):
-        '''test all method'''
-        '''test method for add interaces to router'''
+        '''test method use to add interaces to router'''
         self.assertTrue([]!= r1.interfaces)
 if __name__ == '__main__':
     unittest.main()
