1d2dfe6a2c5808805097f3b471f364a4568f935d
wut5238 <setthawut9915@hotmail.com>
2021-01-30 13:48:32 +0000

    update file router.py and test_router.py

--- router.py
+++ router.py
@@ -1,9 +1,12 @@
-
 class Router:
     def __init__(self, brand, model, hostname):
         self.interfaces = []
         self.brand = brand
         self.model = model
         self.hostname = hostname
+    def add_inf(self, inf):
+        self.interfaces.append(inf)
 r1 = Router('Cisco', 'IOSv', "R1")
 print(r1.hostname)
+r1.add_inf("GigabitEthernet 0/1")
+print(r1.interfaces)
--- test_router.py
+++ test_router.py
@@ -5,5 +5,9 @@ class test_router(unittest.TestCase):
     def test_attribute(self):
         '''test all atribute in router.py'''
         self.assertTrue(self.__init__)
+    def test_method(self):
+        '''test all method'''
+        '''test method for add interaces to router'''
+        self.assertTrue([]!= r1.interfaces)
 if __name__ == '__main__':
     unittest.main()
