81a4912e6b472d7ae901fd4eca0beba39d9841a7
wut5238 <setthawut9915@hotmail.com>
2021-02-09 09:03:45 +0000

    add test_connect to test_router.py

--- test_router.py
+++ test_router.py
@@ -8,6 +8,19 @@ class test_router(unittest.TestCase):
         r1 = Router('Cisco', 'IOSv', "R1")
         r1.add_inf("GigabitEthernet 0/1")
         self.assertTrue([]!= r1.interfaces)
+    
+    def test_connect(self):
+        '''test method connect between interface of router'''
+        r1 = Router('Cisco', 'IOSv', "R1")
+        r1.add_inf("GigabitEthernet 0/1")
+        r1.add_inf("GigabitEthernet 0/2")
+
+        r2 = Router('Cisco', 'IOSv', "R2")
+        r2.add_inf("GigabitEthernet 0/2")
+
+        r1.connect("GigabitEthernet 0/1", r2, "GigabitEthernet 0/2")
+        self.assertTrue({} != r2.connection)
+        self.assertTrue(list(r2.connection.keys())[0] == "R1")
 
 if __name__ == '__main__':
     unittest.main()
